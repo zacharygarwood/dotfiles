98c69ba2a13f8ce3499adc3ca718ee57e96585cd
Zachary Garwood <garwoodzachary@gmail.com>
2024-08-26 19:46:08 -0500

    Remove the .wezterm.lua symlink to windows

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,1 +1,18 @@
-/mnt/c/Users/Zachary/.wezterm.lua
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Set default domain to WSL2
+config.default_domain = 'WSL:Ubuntu'
+
+-- This is where you actually apply your config choices
+
+-- Make the terminal *fancy* 
+config.color_scheme = 'Kanagawa Dragon (Gogh)'
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+
+-- and finally, return the configuration to wezterm
+return config
