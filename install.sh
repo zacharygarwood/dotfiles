bc37c4ea5c7410504b47445cf7177ab546bf0321
Zachary Garwood <garwoodzachary@gmail.com>
2024-08-27 19:37:54 -0500

    Updated install script to create bashrc and source it

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 CURRENT_DIR="dotfiles"
+BASHRC="$HOME/.bashrc"
 
 echo "[$CURRENT_DIR] Beginning installation..."
+
+# Install the files that are used across tools
+if [ ! -f "$BASHRC" ]; then
+    echo "[$CURRENT_DIR] Creating $BASHRC as it did not exist..."
+    touch "$BASHRC"
+fi
+
+# Go through all tools and install them
 for entry in ./*; do
     if [ -d "$entry" ]; then
         if [ -f "$entry/install.sh" ]; then
@@ -12,3 +21,6 @@ for entry in ./*; do
         fi
     fi
 done
+
+# Source the necessary files
+source "$BASHRC"
