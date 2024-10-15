62bdf8c05b9321374658c4e3aacaa469f115ef37
Zachary Garwood <garwoodzachary@gmail.com>
2024-10-14 19:50:28 -0500

    Formatted and added go LSP

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,19 +1,19 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- Set default domain to WSL2
-config.default_domain = 'WSL:Ubuntu'
+config.default_domain = "WSL:Ubuntu"
 
 -- This is where you actually apply your config choices
 config.keys = {
-    { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
 }
--- Make the terminal *fancy* 
-config.color_scheme = 'Kanagawa Dragon (Gogh)'
+-- Make the terminal *fancy*
+config.color_scheme = "Kanagawa Dragon (Gogh)"
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 
